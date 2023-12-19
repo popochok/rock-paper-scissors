c28588cef66ca6e99af748331fb392129173f5ab
Timur <polshamangood@gmail.com>
2023-12-19 13:45:11 -0800

    rock-paper-scissors

---
+++ index.js
@@ -0,0 +1,40 @@
+import readline from 'readline-sync';
+import _ from 'lodash';
+
+const selections = [
+  'камень',
+  'ножницы',
+  'бумага',
+];
+
+const rates = {
+  камень: {
+    ножницы: 'Вы победили!',
+    бумага: 'Вы проиграли!',
+  },
+
+  ножницы: {
+    бумага: 'Вы победили!',
+    камень: 'Вы проиграли!',
+  },
+
+  бумага: {
+    камень: 'Вы победили!',
+    ножницы: 'Вы проиграли!',
+  },
+};
+
+const runGame = () => {
+  const userSelection = readline.question('Выберите, что будете кидать. (камень, ножницы, бумага) ==> ').toLowerCase();
+  const randomSelection = _.sample(selections);
+
+  const result = rates[userSelection][randomSelection];
+
+  if (result === undefined) {
+    return `У вас ничья! У обоих ${userSelection}`;
+  }
+
+  return `${result} \nУ вас ${userSelection}, у программы ${randomSelection}`;
+};
+
+console.log(runGame());
